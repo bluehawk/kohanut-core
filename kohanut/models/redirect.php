92b66e68bbf96ee55df61fb337718151732b8d7f
phroughy@gmail.com <Michael@.(none)>
2009-11-05 21:57:51 -0700

    Creating branch for kohana 3.x

--- kohanut/models/redirect.php
+++
@@ -1,8 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct access allowed.');
-
-class Redirect_Model extends ORM {
-
-
-}
-
-?>
--- kohanut/views/kohanut/admin/error.php
+++
@@ -1,6 +0,0 @@
-<div id="full">
-	<div id="error">
-		<h3>Error:</h3>
-		<p><?php echo $message ?></p>
-	</div>
-</div>
